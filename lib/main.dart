7e5ac89241e063f12b9a588f2d3961700fc43044
Corentin <corentin.mathieu@simplifia.fr>
2022-02-10 14:51:30 +0100

    Base project

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/views/homepage.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Weather App",
+    home: HomePage(),
+  ));
+}
---
+++ lib/views/homepage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:weather_app/widgets/navbar.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  late String villeName = "Paris";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        drawer: NavBar(),
+        appBar: AppBar(
+          title: Text(
+            villeName,
+            textAlign: TextAlign.center,
+            style: const TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+        ),
+        body: Center(
+            child: Column(
+          children: const [],
+        )));
+  }
+}
