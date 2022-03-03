4a079ddf479d9663f3a9bfd6828d784d83b9f0e3
Corentin <corentin.mathieu@simplifia.fr>
2022-03-03 14:41:52 +0100

    DB Connect

---
+++ lib/models/ville.dart
@@ -0,0 +1,37 @@
+final String tableVilles = 'Villes';
+
+class VilleFields {
+  static final List<String> values = [id, name];
+
+  static final String id = '_id';
+  static final String name = 'title';
+}
+
+class Ville {
+  final int? id;
+  final String name;
+
+  const Ville({
+    this.id,
+    required this.name,
+  });
+
+  Ville copy({
+    int? id,
+    String? name
+  }) =>
+      Ville(
+        id: id ?? this.id,
+        name: name ?? this.name,
+      );
+
+  static Ville fromJson(Map<String, Object?> json) => Ville(
+    id: json[VilleFields.id] as int?,
+    name: json[VilleFields.name] as String
+  );
+
+  Map<String, Object?> toJson() => {
+    VilleFields.id: id,
+    VilleFields.name: name,
+  };
+}
